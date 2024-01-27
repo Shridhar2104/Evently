e08dce8833ae2b0c697d9b87fd8a4616573fffc9
Shridhar <shridhar2104@gmail.com>
2024-01-27 12:02:57 +0530

    commit

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useCallback, Dispatch, SetStateAction } from 'react'
-import {type FileWithPath}  from '@uploadthing/react'
+import type { FileWithPath } from '@uploadthing/react'
 import { useDropzone } from '@uploadthing/react/hooks'
 import { generateClientDropzoneAccept } from 'uploadthing/client'
 
@@ -53,4 +53,4 @@ export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploader
       )}
     </div>
   )
-}
+} 
